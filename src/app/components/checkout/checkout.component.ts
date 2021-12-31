e632c783a0b7628a37983a83291dd80f01b3616b
Veeteq <witold.wojnarowicz@gmail.com>
2021-12-31 14:50:04 +0100

    F005 - Send email receipt from Stripe

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -165,6 +165,7 @@ export class CheckoutComponent implements OnInit {
     //compute payment infor for Stripe
     this.paymentInfo.amount = Math.round(this.totalValue * 100);
     this.paymentInfo.currency = 'USD';
+    this.paymentInfo.receiptEmail = purchase.customer.email;
 
     if (!this.checkoutFormGroup.invalid && this.displayError.textContent === "") {
 
